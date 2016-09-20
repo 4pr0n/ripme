373f1d88b58289384a80728612a93a158681cae5
Sleaze <root@dev.null>
2016-09-20 12:53:33 -0700

    Cookie support for Xhamster gallery ripper.

--- src/main/java/com/rarchives/ripme/ripper/rippers/XhamsterRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/XhamsterRipper.java
@@ -3,6 +3,8 @@ package com.rarchives.ripme.ripper.rippers;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,6 +18,8 @@ import com.rarchives.ripme.utils.Utils;
 public class XhamsterRipper extends AlbumRipper {
 
     private static final String HOST = "xhamster";
+
+    private HashMap<String, Document> docs = new HashMap<String, Document>();
 
     public XhamsterRipper(URL url) throws IOException {
         super(url);
@@ -39,7 +43,7 @@ public class XhamsterRipper extends AlbumRipper {
         String nextURL = this.url.toExternalForm();
         while (nextURL != null) {
             logger.info("    Retrieving " + nextURL);
-            Document doc = Http.url(nextURL).get();
+            Document doc = Http.url(nextURL).header("User-Agent", USER_AGENT).referrer("http://" + HOST + ".com/").cookies(Utils.getCookies(HOST)).get();
             for (Element thumb : doc.select("table.iListing div.img img")) {
                 if (!thumb.hasAttr("src")) {
                     continue;
@@ -73,6 +77,36 @@ public class XhamsterRipper extends AlbumRipper {
         waitForThreads();
     }
 
+    /**
+     * @todo prefix with uploader username
+     */
+    @Override
+    public String getAlbumTitle(URL url) throws MalformedURLException {
+        String title = HOST + "_";
+        Document doc = docs.get(url);;
+        if (doc == null) {
+            try {
+                doc = Http.url(url).header("User-Agent", USER_AGENT).referrer(url).cookies(Utils.getCookies(HOST)).get();
+                docs.put(url.toString(), doc);
+            } catch (IOException e) {
+                logger.error("Failed to download url=" + url + ": " + e.getMessage());
+            }
+        }
+        // Find username.
+        if (doc != null) {
+            for (Element link : doc.select("#galleryUser .item a")) {
+                title += link.text() + "_";
+                break;
+            }
+        }
+        String galleryLink = url.toExternalForm();
+        title += galleryLink
+            .replaceFirst("^http.*/photos/(?:gallery/([^?#:&]+)|view/([^-]+)-).*$", "$1$2")
+            .replace('/', '-')
+            .replace(".html", "");
+        return title;
+    }
+
     @Override
     public String getHost() {
         return HOST;
@@ -80,15 +114,14 @@ public class XhamsterRipper extends AlbumRipper {
 
     @Override
     public String getGID(URL url) throws MalformedURLException {
-        Pattern p = Pattern.compile("^https?://([a-z0-9.]*?)xhamster\\.com/photos/gallery/([0-9]{1,})/.*\\.html");
-        Matcher m = p.matcher(url.toExternalForm());
-        if (m.matches()) {
-            return m.group(2);
-        }
-        throw new MalformedURLException(
+        String gid = url.toExternalForm().replaceFirst("^https?://(?:[a-z0-9.]*?)" + HOST + "\\.com/photos/(?:gallery/([0-9]{1,})/.*\\.html|view/([^-]+)-).*$", "$1$2");
+        if (gid.length() == 0) {
+            throw new MalformedURLException(
                 "Expected xhamster.com gallery formats: "
-                        + "xhamster.com/photos/gallery/#####/xxxxx..html"
-                        + " Got: " + url);
+                + "http://xhamster.com/photos/gallery/#####/xxxxx..html or http://xhamster.com/photos/view/####-####.html"
+                + " Got: " + url);
+        }
+        return gid;
     }
 
 }
