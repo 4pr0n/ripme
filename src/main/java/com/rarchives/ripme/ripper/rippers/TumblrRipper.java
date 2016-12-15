2100f1501e5893e169dea6db814ad882105bfa50
John Gibson <jgibson02@users.noreply.github.com>
2016-12-14 22:22:18 -0500

    Updated working tumblr auth key

--- src/main/java/com/rarchives/ripme/ripper/rippers/TumblrRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/TumblrRipper.java
@@ -29,7 +29,7 @@ public class TumblrRipper extends AlbumRipper {
 
     private static final String API_KEY;
     static {
-        API_KEY = Utils.getConfigString("tumblr.auth", "v5kUqGQXUtmF7K0itri1DGtgTs0VQpbSEbh1jxYgj9d2Sq18F8");
+        API_KEY = Utils.getConfigString("tumblr.auth", "HFLbdpEE0exDQVcxcLJiC2D6iwYjZdJHT2rLgObI7lvmmbBpha");
     }
 
     public TumblrRipper(URL url) throws IOException {
