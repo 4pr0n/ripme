763e90279b92d7ccc393036dad570c1e411e8c23
Gaboso <gabrielsantiago2@gmail.com>
2017-05-18 14:45:12 -0400

    Changed logger to maintain the application's current naming pattern. Bringing back the braces. Created constant for "m.imgur.com" Removed line breaks in urlFromDirectory method. Also created constant for initial url of imgur albums.

--- src/main/java/com/rarchives/ripme/utils/RipUtils.java
+++ src/main/java/com/rarchives/ripme/utils/RipUtils.java
@@ -24,9 +24,11 @@ import java.util.regex.Pattern;
 
 public class RipUtils {
 
-    private static final Logger LOGGER = Logger.getLogger(RipUtils.class);
-    private static final String IMGUR_COM = "imgur.com";
+    private static final Logger logger = Logger.getLogger(RipUtils.class);
     private static final String HTTP = "http://";
+    private static final String IMGUR_COM = "imgur.com";
+    private static final String M_IMGUR_COM = "m." + IMGUR_COM;
+    private static final String IMGUR_ALBUM_START_URL = IMGUR_COM + "/a/";
 
     private RipUtils() {
     }
@@ -34,62 +36,62 @@ public class RipUtils {
     public static List<URL> getFilesFromURL(URL url) {
         List<URL> result = new ArrayList<URL>();
 
-        LOGGER.debug("Checking " + url);
+        logger.debug("Checking " + url);
         // Imgur album
-        if ((url.getHost().endsWith(IMGUR_COM)) && url.toExternalForm().contains("imgur.com/a/")) {
+        if ((url.getHost().endsWith(IMGUR_COM)) && url.toExternalForm().contains(IMGUR_ALBUM_START_URL)) {
             try {
-                LOGGER.debug("Fetching imgur album at " + url);
+                logger.debug("Fetching imgur album at " + url);
                 ImgurAlbum imgurAlbum = ImgurRipper.getImgurAlbum(url);
                 for (ImgurImage imgurImage : imgurAlbum.images) {
-                    LOGGER.debug("Got imgur image: " + imgurImage.url);
+                    logger.debug("Got imgur image: " + imgurImage.url);
                     result.add(imgurImage.url);
                 }
             } catch (IOException e) {
-                LOGGER.error("[!] Exception while loading album: " + url, e);
+                logger.error("[!] Exception while loading album: " + url, e);
             }
 
             return result;
         } else if (url.getHost().endsWith(IMGUR_COM) && url.toExternalForm().contains(",")) {
             // Imgur image series.
             try {
-                LOGGER.debug("Fetching imgur series at " + url);
+                logger.debug("Fetching imgur series at " + url);
                 ImgurAlbum imgurAlbum = ImgurRipper.getImgurSeries(url);
                 for (ImgurImage imgurImage : imgurAlbum.images) {
-                    LOGGER.debug("Got imgur image: " + imgurImage.url);
+                    logger.debug("Got imgur image: " + imgurImage.url);
                     result.add(imgurImage.url);
                 }
             } catch (IOException e) {
-                LOGGER.error("[!] Exception while loading album: " + url, e);
+                logger.error("[!] Exception while loading album: " + url, e);
             }
         } else if (url.getHost().endsWith("gfycat.com")) {
             try {
-                LOGGER.debug("Fetching gfycat page: " + url);
+                logger.debug("Fetching gfycat page: " + url);
                 String videoURL = GfycatRipper.getVideoURL(url);
-                LOGGER.debug("Got gfycat URL: " + videoURL);
+                logger.debug("Got gfycat URL: " + videoURL);
                 result.add(new URL(videoURL));
             } catch (IOException e) {
                 // Do nothing
-                LOGGER.warn("Exception while retrieving gfycat page: " + url, e);
+                logger.warn("Exception while retrieving gfycat page: " + url, e);
             }
 
             return result;
         } else if (url.toExternalForm().contains("vidble.com/album/") || url.toExternalForm().contains("vidble.com/show/")) {
             try {
-                LOGGER.info("Getting vidble album " + url);
+                logger.info("Getting vidble album " + url);
                 result.addAll(VidbleRipper.getURLsFromPage(url));
             } catch (IOException e) {
                 // Do nothing
-                LOGGER.warn("Exception while retrieving vidble page: " + url, e);
+                logger.warn("Exception while retrieving vidble page: " + url, e);
             }
 
             return result;
         } else if (url.toExternalForm().contains("eroshare.com")) {
             try {
-                LOGGER.info("Getting eroshare album " + url);
+                logger.info("Getting eroshare album " + url);
                 result.addAll(EroShareRipper.getURLs(url));
             } catch (IOException e) {
                 // Do nothing
-                LOGGER.warn("Exception while retrieving eroshare page: " + url, e);
+                logger.warn("Exception while retrieving eroshare page: " + url, e);
             }
 
             return result;
@@ -98,13 +100,13 @@ public class RipUtils {
         Pattern p = Pattern.compile("https?://i.reddituploads.com/([a-zA-Z0-9]+)\\?.*");
         Matcher m = p.matcher(url.toExternalForm());
         if (m.matches()) {
-            LOGGER.info("URL: " + url.toExternalForm());
+            logger.info("URL: " + url.toExternalForm());
             String u = url.toExternalForm().replaceAll("&amp;", "&");
 
             try {
                 result.add(new URL(u));
             } catch (MalformedURLException e) {
-                LOGGER.error("[!] Not a valid URL: '" + u + "'", e);
+                logger.error("[!] Not a valid URL: '" + u + "'", e);
             }
 
             return result;
@@ -116,15 +118,15 @@ public class RipUtils {
         if (m.matches()) {
             try {
                 URL singleURL = new URL(m.group(1));
-                LOGGER.debug("Found single URL: " + singleURL);
+                logger.debug("Found single URL: " + singleURL);
                 result.add(singleURL);
                 return result;
             } catch (MalformedURLException e) {
-                LOGGER.error("[!] Not a valid URL: '" + url + "'", e);
+                logger.error("[!] Not a valid URL: '" + url + "'", e);
             }
         }
 
-        if (url.getHost().equals(IMGUR_COM) || "m.imgur.com".equals(url.getHost())) {
+        if (url.getHost().equals(IMGUR_COM) || M_IMGUR_COM.equals(url.getHost())) {
             try {
                 // Fetch the page
                 Document doc = Jsoup.connect(url.toExternalForm()).userAgent(AbstractRipper.USER_AGENT).get();
@@ -136,11 +138,11 @@ public class RipUtils {
                     }
                 }
             } catch (IOException ex) {
-                LOGGER.error("[!] Error", ex);
+                logger.error("[!] Error", ex);
             }
         }
 
-        LOGGER.error("[!] Unable to rip URL: " + url);
+        logger.error("[!] Unable to rip URL: " + url);
         return result;
     }
 
@@ -151,79 +153,70 @@ public class RipUtils {
     public static String urlFromDirectoryName(String dir) {
         String url = urlFromImgurDirectoryName(dir);
 
-        if (url == null)
-            url = urlFromImagefapDirectoryName(dir);
-        if (url == null)
-            url = urlFromDeviantartDirectoryName(dir);
-        if (url == null)
-            url = urlFromRedditDirectoryName(dir);
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "bfcakes", "http://www.bcfakes.com/celebritylist/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "butttoucher", "http://butttoucher.com/users/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "cheeby", "http://cheeby.com/u/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "datwin", "http://datw.in/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "drawcrowd", "http://drawcrowd.com/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir.replace("-", "/"), "ehentai", "http://g.e-hentai.org/g/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "fapproved", "http://fapproved.com/users/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "vinebox", "http://finebox.co/u/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "imgbox", "http://imgbox.com/g/", "");
-        if (url == null)
-            url = urlFromSiteDirectoryName(dir, "modelmayhem", "http://www.modelmayhem.com/", "");
+        if (url == null) url = urlFromImagefapDirectoryName(dir);
+        if (url == null) url = urlFromDeviantartDirectoryName(dir);
+        if (url == null) url = urlFromRedditDirectoryName(dir);
+        if (url == null) url = urlFromSiteDirectoryName(dir, "bfcakes", "http://www.bcfakes.com/celebritylist/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "butttoucher", "http://butttoucher.com/users/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "cheeby", "http://cheeby.com/u/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "datwin", "http://datw.in/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "drawcrowd", "http://drawcrowd.com/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir.replace("-", "/"), "ehentai", "http://g.e-hentai.org/g/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "fapproved", "http://fapproved.com/users/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "vinebox", "http://finebox.co/u/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "imgbox", "http://imgbox.com/g/", "");
+        if (url == null) url = urlFromSiteDirectoryName(dir, "modelmayhem", "http://www.modelmayhem.com/", "");
 
         //if (url == null) url = urlFromSiteDirectoryName(dir, "8muses",      "http://www.8muses.com/index/category/", "");
         return url;
     }
 
     private static String urlFromSiteDirectoryName(String dir, String site, String before, String after) {
-        if (!dir.startsWith(site + "_"))
+        if (!dir.startsWith(site + "_")) {
             return null;
+        }
 
         String directory = dir.substring((site + "_").length());
         return before + directory + after;
     }
 
     private static String urlFromRedditDirectoryName(String dir) {
-        if (!dir.startsWith("reddit_"))
+        if (!dir.startsWith("reddit_")) {
             return null;
+        }
 
         String url = null;
         String[] fields = dir.split("_");
 
-        if ("sub".equals(fields[0]))
+        if ("sub".equals(fields[0])) {
             url = "http://reddit.com/r/" + dir;
-        else if ("user".equals(fields[0]))
+        } else if ("user".equals(fields[0])) {
             url = "http://reddit.com/user/" + dir;
-        else if ("post".equals(fields[0]))
+        } else if ("post".equals(fields[0])) {
             url = "http://reddit.com/comments/" + dir;
-
+        }
         return url;
     }
 
     private static String urlFromImagefapDirectoryName(String dir) {
-        if (!dir.startsWith("imagefap"))
+        if (!dir.startsWith("imagefap")) {
             return null;
+        }
 
         String url;
         String directory = dir.substring("imagefap_".length());
-        if (NumberUtils.isDigits(directory))
+        if (NumberUtils.isDigits(directory)) {
             url = "http://www.imagefap.com/gallery.php?gid=" + directory;
-        else
+        } else {
             url = "http://www.imagefap.com/gallery.php?pgid=" + directory;
-
+        }
         return url;
     }
 
     private static String urlFromDeviantartDirectoryName(String dir) {
-        if (!dir.startsWith("deviantart"))
+        if (!dir.startsWith("deviantart")) {
             return null;
+        }
 
         String directory = dir.substring("deviantart_".length());
         String url;
@@ -238,12 +231,14 @@ public class RipUtils {
     }
 
     private static String urlFromImgurDirectoryName(String dir) {
-        if (!dir.startsWith("imgur_"))
+        if (!dir.startsWith("imgur_")) {
             return null;
+        }
 
         String directory = dir;
-        if (dir.contains(" "))
+        if (dir.contains(" ")) {
             directory = dir.substring(0, dir.indexOf(' '));
+        }
 
         List<String> fields = Arrays.asList(directory.split("_"));
         String album = fields.get(1);
@@ -261,18 +256,18 @@ public class RipUtils {
                 sub = fields.remove(0); // Subreddit that may contain "_"
             }
 
-            url += "imgur.com/r/" + sub + "/";
+            url += IMGUR_COM + "/r/" + sub + "/";
             url += fields.remove(0) + "/";
             url += fields.remove(0);
         } else if (album.contains("-")) {
             // Series of images
-            url += "imgur.com/" + album.replaceAll("-", ",");
+            url += IMGUR_COM + "/" + album.replaceAll("-", ",");
         } else if (album.length() == 5 || album.length() == 6) {
             // Album
-            url += "imgur.com/a/" + album;
+            url += IMGUR_ALBUM_START_URL + album;
         } else {
             // User account
-            url += album + ".imgur.com/";
+            url += album + "." + IMGUR_COM + "/";
             if (fields.size() > 2) {
                 url += fields.get(2);
             }
